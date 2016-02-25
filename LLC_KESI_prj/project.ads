4c4250b37dc26e3dc6d824c1fb8f2f6443d66fad
fanoro m <adeniyicall@gmail.com>
2016-02-25 23:13:49 +0200

    new CETEST FOLDER

---
+++ LLC_KESI_prj/project.ads
@@ -0,0 +1,34 @@
+<ADSProject Revision="2" Version="100">
+    <Project Name="">
+        <Design Name="DC_CURVE_MODIFIER_1.dsn" />
+        <Design Name="llc.dsn" />
+        <Design Name="llc_main.dsn" />
+        <Design Name="llc_modify_1.dsn" />
+        <Design Name="llc_modify_1_with_port.dsn" />
+        <Design Name="llc_modify_1_with_port_KESI.dsn" />
+        <Design Name="llc_modify_1_with_port_KESI78.dsn" />
+        <Design Name="llc_modify_1_with_port_original.dsn" />
+        <Design Name="llc_modify_1_without_port.dsn" />
+        <Design Name="port_dc_curver.dsn" />
+        <Ael Name="save_project_state.ael" />
+        <Data_Display Name="llc.dds" />
+        <Data_Display Name="llc_main.dds" />
+        <Data_Display Name="llc_modify_1_sopt.dds" />
+        <Data_Display Name="llc_SP.dds" />
+        <Dataset Name="llc.ds" />
+        <Dataset Name="llc_main.ds" />
+        <Dataset Name="llc_modify_1_with_port.ds" />
+        <Dataset Name="llc_modify_1_with_port_KESI78.ds" />
+        <Dataset Name="llc_modify_1_with_port_untitled.ds" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="mm_layout.prf" />
+        <Preferences Name="mm_schematic.prf" />
+        <Preferences Name="schematic.prf" />
+        <Text Name="sessloghpeesofsim3.txt" />
+        <Dataset Name="port_dc_curver.ds" />
+        <Dataset Name="llc_modify_1.ds" />
+        <Design Name="another_curve_dc.dsn" />
+        <Design Name="dc_curves.dsn" />
+        <Data_Display Name="llc_modify_1_with_port_KESI78.dds" />
+    </Project>
+</ADSProject>
